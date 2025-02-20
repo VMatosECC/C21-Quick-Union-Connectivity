407c78ce218f92c6c43cdeffb4f6520bb30fce9e
Victor Matos <vmatos@elcamino.edu>
2025-02-20 07:11:22 -0800

    print replaced by toString

--- C21-Quick-Union-Connectivity.cpp
+++ C21-Quick-Union-Connectivity.cpp
@@ -1,23 +1,16 @@
 // C21-Quick-Union-Connectivity-App.cpp 
 /*
-    Quick Find and Quick Union are two algorithms to solve the connectivity problem.
-    -The connectivity problem is to determine if two elements are connected in a set.
-    -The set can be represented as an array of integers where the index represents the element
-    and the value represents the group.
-    -The connectivity problem is to determine if two elements are in the same group.
-    -The Quick Find algorithm is simple but inefficient.
-    -The Quick Union algorithm is more efficient than Quick Find.
-    -The Quick Union algorithm uses a tree data structure to represent the set.
+Quick-union uses a tree-based structure to represent the connected components of a graph.
     -The root of the tree represents the group.
     -The Quick Union algorithm is more efficient than Quick Find because
-    it uses the root of the tree to determine if two elements are in the same group.
-    -The Quick Union algorithm has a time complexity of O(N) for union and find operations
-     worst case scenario.
+     it uses the root of the tree to determine if two elements are in the same group.
+    -The Quick Union algorithm has a time complexity of O(N) for union and find operations.
 
-    References: https://algs4.cs.princeton.edu/15uf/
+References: https://algs4.cs.princeton.edu/15uf/
 
 */
 #include <iostream>
+#include <sstream>
 #include <vector>
 using namespace std;
 
@@ -51,11 +44,13 @@ public:
         id[i] = j;
     }
 
-    void print() {
+    string toString() {
+        stringstream sout;
         for (int i = 0; i < id.size(); i++) {
-            cout << id[i] << " ";
+            sout << id[i] << " ";
         }
-        cout << endl;
+        sout << endl;
+        return sout.str();
     }
 };
 
@@ -64,42 +59,42 @@ int main() {
     QuickUnion qu(10);
 
     cout << "Initial state:" << endl;
-    qu.print();
+    cout << qu.toString();
 
     qu.union_elements(4, 3);
     cout << "After union(4, 3):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     qu.union_elements(3, 8);
     cout << "After union(3, 8):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     qu.union_elements(6, 5);
     cout << "After union(6, 5):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     qu.union_elements(9, 4);
     cout << "After union(9, 4):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     qu.union_elements(2, 1);
     cout << "After union(2, 1):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     cout << "Find(8, 9): " << (qu.find(8, 9) ? "True" : "False") << endl;
     cout << "Find(5, 4): " << (qu.find(5, 4) ? "True" : "False") << endl;
 
     qu.union_elements(5, 0);
     cout << "After union(5, 0):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     qu.union_elements(7, 2);
     cout << "After union(7, 2):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     qu.union_elements(6, 1);
     cout << "After union(6, 1):" << endl;
-    qu.print();
+    cout << qu.toString();
 
     cout << "Find(1, 0): " << (qu.find(1, 0) ? "True" : "False") << endl;
 
